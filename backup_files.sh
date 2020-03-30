dd1ac621d3c5129e9739ae2899cfa3f459d3b5d1
akira <akirahinoshiro@gmail.com>
2020-03-30 11:13:13 +0200

    added qt5/gtk2/3 theme configs

--- backup_files.sh
+++ backup_files.sh
@@ -16,9 +16,21 @@ mkdir -p .config/tint2
 cp ~/.config/tint2/tint2rc .config/tint2/
 
 #fluxbox
-mkdir .fluxbox
+mkdir -p .fluxbox
 cp ~/.fluxbox/init .fluxbox/
 cp ~/.fluxbox/keys .fluxbox/
 cp ~/.fluxbox/menu .fluxbox/
 cp ~/.fluxbox/random_wallpaper .fluxbox/
 cp ~/.fluxbox/startup .fluxbox/
+
+#gtk
+mkdir -p .config/gtk-3.0/
+cp ~/.config/gtk-3.0/settings.ini .config/gtk-3.0/
+cp ~/.gtkrc-2.0 .
+
+#qt5
+mkdir -p .config/qt5ct
+cp ~/.config/qt5ct/qt5ct.conf .config/qt5ct/
+
+#misc
+cp ~/.bashrc .
