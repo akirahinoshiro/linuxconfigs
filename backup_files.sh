75ecbccaf4267d3a19fcf2baee3fe7c4815f66b6
akira <akirahinoshiro@gmail.com>
2020-03-31 16:29:35 +0200

    obmenu changed more or less thats all

--- backup_files.sh
+++ backup_files.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
 #openbox
-mkdir -p .config/openbox
-cp ~/.config/openbox/autostart .config/openbox/
-cp ~/.config/openbox/menu.xml .config/openbox/
-cp ~/.config/openbox/rc.xml .config/openbox/
+mkdir -p home/.config/openbox
+cp ~/.config/openbox/autostart home/.config/openbox/
+cp ~/.config/openbox/menu.xml home/.config/openbox/
+cp ~/.config/openbox/rc.xml home/.config/openbox/
+mkdir -p home/.config/obmenu-generator
+cp ~/.config/obmenu-generator/schema.pl home/.config/obmenu-generator
 
 #vscode
-mkdir -p .config/Code/User
-cp ~/.config/Code/User/keybindings.json .config/Code/User/
-cp ~/.config/Code/User/settings.json .config/Code/User/
+mkdir -p home/.config/Code/User
+cp ~/.config/Code/User/keybindings.json home/.config/Code/User/
+cp ~/.config/Code/User/settings.json home/.config/Code/User/
 
 #tint2
-mkdir -p .config/tint2
-cp ~/.config/tint2/tint2rc .config/tint2/
+mkdir -p home/.config/tint2
+cp ~/.config/tint2/tint2rc home/.config/tint2/
 
 #fluxbox
-mkdir -p .fluxbox
-cp ~/.fluxbox/init .fluxbox/
-cp ~/.fluxbox/keys .fluxbox/
-cp ~/.fluxbox/menu .fluxbox/
-cp ~/.fluxbox/random_wallpaper .fluxbox/
-cp ~/.fluxbox/startup .fluxbox/
+mkdir -p home/.fluxbox
+cp ~/.fluxbox/init home/.fluxbox/
+cp ~/.fluxbox/keys home/.fluxbox/
+cp ~/.fluxbox/menu home/.fluxbox/
+cp ~/.fluxbox/random_wallpaper home/.fluxbox/
+cp ~/.fluxbox/startup home/.fluxbox/
 
 #gtk
-mkdir -p .config/gtk-3.0/
-cp ~/.config/gtk-3.0/settings.ini .config/gtk-3.0/
+mkdir -p home/.config/gtk-3.0/
+cp ~/.config/gtk-3.0/settings.ini home/.config/gtk-3.0/
 cp ~/.gtkrc-2.0 .
 
 #qt5
-mkdir -p .config/qt5ct
-cp ~/.config/qt5ct/qt5ct.conf .config/qt5ct/
+mkdir -p home/.config/qt5ct
+cp ~/.config/qt5ct/qt5ct.conf home/.config/qt5ct/
 
 #misc
 cp ~/.bashrc .
